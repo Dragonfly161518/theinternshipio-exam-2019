f6b3d9beeeaa71c274f9a386eb3da7f1b44cb7aa
Nattapat <Tatsuya10199@gmail.com>
2019-01-31 23:43:09 +0700

    add gitignore , edit readme

---
+++ weather/README.md
@@ -0,0 +1,30 @@
+## ข้อมูลส่วนตัว
+ชื่อ: นายณัฐภัทร นามสกุล ศรีวิชัยลำพันธ์ <br>
+Email: por.nattapat.01@gmail.com <br>
+Tel: 099-4299325 <br>
+
+## วิธีการ Compile / Build / Run
+
+
+```
+    git clone https://github.com/Dragonfly161518/theinternshipio-exam-2019.git
+```
+
+ทำการ Clone Project ลงในเครื่อง
+
+```
+    cd weather
+```
+
+เข้าไฟล์ Weather
+
+```
+    node main.js [filename]
+```
+
+รันไฟล์ main.js ด้วย nodejs และส่งชื่อไฟล์ผ่าน parameter filename (xml file)
+
+> Output: weather.json (JSON file)
+
+
+
--- weather/main.js
+++ weather/main.js
@@ -1,93 +1,106 @@
-const fs = require('fs')
+// const fs = require('fs')
 
-if(process.argv.length != 3) {
-    console.log("Should send file for parse")
-} else {
-    let xml = fs.readFileSync("./" + process.argv[2], "utf8")
-    xml = xml.split('\r\n')
-    let stack = []
-    let state = 0
-    for(let line of xml) {
-        if(line.indexOf('<?') >= 0) {
-            continue;
-        }
-        line = line.trim()
-        /*
-        for all line 
-        1: <  | collect attribute
-        2: >  | collect data
-        0: </ or /> | next , ready to start new tag 
-        */
-        if(state == 0 && line.indexOf('<') >= 0) {
+// getNameAndValue = (str) => {
+//     let state = 0
+//     let name = []
+//     let value = []
+//     let stateIndex = 0
+//     for(let i=0;i<str.length;i++) {
+//         if(str[i] == " ") {
+//             continue
+//         } else if(state == 0) {
+//             state = 1
+//             stateIndex = 0
+//         }
+//         if (str[i] == '=') {
+//             name.push(str.substring(stateIndex, i))
+//             state = 1
+//             stateIndex = i + 2
+//             let nextEnd = str.indexOf('"',stateIndex)
+//             value.push(str.substring(i+2,nextEnd))
+//             i = nextEnd + 1
+//             state = 0
+//         }
+//     }
+//     return { name , value}
+// }
+
+// if(process.argv.length != 3) {
+//     console.log("Should send file for parse")
+// } else {
+//     var result = {}        
+//     let xml = fs.readFileSync("./" + process.argv[2], "utf8")
+//     xml = xml.split('\r\n')
+//     let stack = []
+//     let state = 0
+//     for(let line of xml) {
+//         if(line.indexOf('<?') >= 0) {
+//             continue;
+//         }
+//         line = line.trim()
+//         /*
+//         for all line 
+//         1: <  | collect attribute
+//         2: >  | collect data
+//         0: </ or /> | next , ready to start new tag 
+//         */
+//         if(line.indexOf('<') >= 0) {
             
-            state = 1;
-            let i = line.indexOf('<')
-            do {
-                i++
-            } while(line[i] != '>' || line[i] != '/>' || line[i] != ' ')
+//             state = 1;
+//             let i = line.indexOf('<')
+//             do {
+//                 i++
+//             } while(line[i] != ">" && line[i] != "/>" && line[i] != " ")
 
-            // set open tag
-            stack.push(line.substring(line.indexOf('<')+1,i))
+//             // set open tag
+//             stack.push(line.substring(line.indexOf('<')+1,i))
 
-            // part collect attribute
-            let endTag = 0
-            let closeOne = line.indexOf('>')
-            let closeTwo = line.indexOf('/>')
-            if( closeOne >= 0) 
-                endTag = closeOne
-            if( closeTwo >= 0) 
-                endTag = closeTwo
-            if(endTag == 0) {
-                // cant find any tag close
-                // <TAG ...
-                //      .... > or />
+//             // part collect attribute
+//             let endTag = 0
+//             let closeOne = line.indexOf('>')
+//             let closeTwo = line.indexOf('/>')
+//             if( closeOne >= 0) 
+//                 endTag = closeOne
+//             if( closeTwo >= 0) 
+//                 endTag = closeTwo
+//             if(endTag == 0) {
+//                 // cant find any tag close
+//                 // <TAG ...
+//                 //      .... > or />
 
-            } else {
-                let somePart = ""
-                if(closeOne >= 0) {
-                    // <TAG ...>
-                    somePart = line.substring(i+1,closeOne)
-                    // finish collect attribute
-                    state = 2
-                } else {
-                    // <TAG .../>
-                    somePart = line.substring(i+1,closeTwo)
-                    // finish collect attribute and start new tag
-                    state = 0
-                }
-                somePart = somePart.trim()
-                let {name,value} = getNameAndValue(somePart)
-                
-            
-            }
+//             } else {
+//                 let somePart = ""
+//                 if(closeOne >= 0) {
+//                     // <TAG ...>
+//                     somePart = line.substring(i+1,closeOne)
+//                     // finish collect attribute
+//                     state = 2
+//                 } else {
+//                     // <TAG .../>
+//                     somePart = line.substring(i+1,closeTwo)
+//                     // finish collect attribute and start new tag
+//                     state = 0
+//                 }
+//                 somePart = somePart.trim()
+//                 let {name,value} = getNameAndValue(somePart)
+//                 console.log(name)
+//                 console.log(value)
+//                 for(let i = 0 ;i < name.length ; i++) {
+//                     const a = name[i]
+//                     const b = value[i]
+//                     result['test']['while'] = 'a'
+//                 }
+//             }
 
 
-        }
+//         }
 
-    }
-}
+//     }
+//     console.log(stack)
+//     result['test'] = {
+//         "id": "123456"
+//     }
+//     result['test']['while'] = 'a'
+//     console.log(JSON.stringify(result))
+// }
 
-getNameAndValue = (str) => {
-    let state = 0
-    let name = []
-    let value = []
-    let stateIndex = 0
-    for(let i=0;i<str.length;i++) {
-        if(str[i] == " ") {
-            continue
-        } else if(state == 0) {
-            state = 1
-            stateIndex = 0
-        }
-        if (str[i] == '=') {
-            name.push(str.substring(stateIndex, i))
-            state = 1
-            stateIndex = i + 2
-            let nextEnd = str.indexOf('"',stateIndex)
-            value.push(str.substring(i+2,nextEnd))
-            i = nextEnd + 1
-            state = 0
-        }
-    }
-    return { name , value}
-}
